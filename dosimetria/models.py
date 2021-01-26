ae35bb23b714af2216fe6b3e845bc20887144183
DABND19 <grigorenkodavid@gmail.com>
2021-01-26 11:47:42 +0300

    Первая версия структуры БД. Наверное, лучше привязать некоторые сущности не к кварталу, а к дате.

--- dosimetria/models.py
+++ dosimetria/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+import main.models as main
 
-# Create your models here.
+
+class Client(models.Model):
+    """ИДК клиент"""
+    organisation = models.ForeignKey(
+        to=main.Organisation,
+        on_delete=models.CASCADE
+    )
+
+    DTU_num = models.PositiveSmallIntegerField()
+    DTL_num = models.PositiveSmallIntegerField()
+
+
+class Address(models.Model):
+    """Адрес ИДК клиента"""
+    organisation = models.ForeignKey(
+        to=Client,
+        on_delete=models.CASCADE
+    )
+    address = models.OneToOneField(
+        to=main.Address,
+        on_delete=models.PROTECT
+    )
+
+
+class Contract(models.Model):
+    """Контактная информация ИДК клиента"""
+    organisation = models.ForeignKey(to=Client, on_delete=models.PROTECT)
+    begin = models.DateField()
+    end = models.DateField()
+
+
+class Quarter(models.Model):
+    """Квартальная отчетность"""
+    year = models.PositiveSmallIntegerField()
+    number = models.PositiveSmallIntegerField()
+
+    begin = models.DateField()
+    end = models.DateField()
+
+    total = models.PositiveBigIntegerField(null=True)
+
+
+class Protocol(models.Model):
+    """Протокол проведенных работ с ИДК клиентом"""
+    organisation = models.ForeignKey(
+        to=Client,
+        on_delete=models.PROTECT
+    )
+    quarter = models.ForeignKey(
+        to=Quarter,
+        on_delete=models.PROTECT
+    )
+
+    number = models.CharField(max_length=32)
+
+
+class Document(models.Model):
+    """Документ, переданный клиенту в определенный квартал"""
+    organisation = models.ForeignKey(
+        to=Client,
+        on_delete=models.PROTECT
+    )
+    quarter = models.ForeignKey(
+        to=Quarter,
+        on_delete=models.PROTECT
+    )
+
+    name = models.CharField(max_length=256)
+    is_returned = models.BooleanField(default=False)
+
+
+class Invoice(models.Model):
+    """Счет, выставленный ИДК клиенту в определенный квартал"""
+    organisation = models.ForeignKey(
+        to=Client,
+        on_delete=models.PROTECT
+    )
+    quarter = models.ForeignKey(
+        to=Quarter,
+        on_delete=models.PROTECT
+    )
+
+    number = models.CharField(max_length=32)
+    total = models.PositiveIntegerField()
+    paid_at = models.DateField(null=True)
+    is_paid = models.BooleanField(default=False)
--- main/models.py
+++ main/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 
-class Сounterparty(models.Model):
-    name = models.CharField(max_length=256)
+class Organisation(models.Model):
+    """Здесь храним все организации"""
+    name = models.CharField(
+        max_length=256,
+        unique=True
+    )
     taxpayer_id = models.CharField(
-        max_length=12, 
+        max_length=12,
+        unique=True,
         validators=[
             RegexValidator(
                 regex=r'\d{12}',
@@ -13,4 +18,45 @@ class Сounterparty(models.Model):
                 code='invalid_taxpayer_id'
             )
         ]
+    )
+
+
+class Address(models.Model):
+    """Здесь храним все адреса"""
+    owner = models.ForeignKey(to=Organisation, on_delete=models.CASCADE)
+    detail = models.CharField(max_length=256)
+
+
+class LegalAddress(models.Model):
+    """Здесь храним юридический адрес"""
+    organisation = models.ForeignKey(
+        to=Organisation,
+        on_delete=models.CASCADE
+    )
+    address = models.OneToOneField(
+        to=Address,
+        on_delete=models.CASCADE
+    )
+
+
+class Contact(models.Model):
+    """Здесь храним все контакты"""
+    organisation = models.ForeignKey(
+        to=Organisation,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=128, unique=True)
+    phone = models.CharField(max_length=16, null=True)
+    email = models.EmailField(null=True)
+
+
+class LegalContact(models.Model):
+    """Здесь храним контакты доверенных лиц организации"""
+    organisation = models.ForeignKey(
+        to=Organisation,
+        on_delete=models.CASCADE
+    )
+    contact = models.OneToOneField(
+        to=Contact,
+        on_delete=models.CASCADE
     )
